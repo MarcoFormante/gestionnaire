6ae106a61ea03939b13d889c0dd9cb6f32b9602c
Marco <formante.marco@gmail.com>
2025-09-28 12:01:46 +0200

    Test script with 'list' command and display a message

--- main.php
+++ main.php
@@ -1,5 +1,8 @@
 <?php
 while (true) {
     $line = readline("Entrez votre commande : ");
-    echo "Vous avez saisi : $line\n";
+    
+    if($line === 'list'){
+        echo "affichage de la liste \n";
+    }
 }
