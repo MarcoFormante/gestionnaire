02b5b59d794d75f8a8753dbec6d111943869af29
Marco <formante.marco@gmail.com>
2025-09-28 13:58:05 +0200

    Update main file adding Command methods to handle user commands

--- main.php
+++ main.php
@@ -1,16 +1,12 @@
 <?php
 require_once 'ContactManager.php';
+require_once 'Command.php';
 
 
 $manager = new ContactManager();
+$command = new Command($manager);
 
 while (true) {
-    $line = readline("Entrez votre commande : ");
-    
-    if($line === 'list'){
-        $contacts = $manager->findAll();
-        foreach ($contacts as $contact) {
-            echo $contact;
-        }
-    }
+    $command->start();
+    $command->handleCommand(($command->line));
 }
