58eee293412a1cd00631a0a54787f92ffa303271
Marco <formante.marco@gmail.com>
2025-09-28 13:04:12 +0200

    Add ContactManager and fetch all contacts with 'list' command

--- main.php
+++ main.php
@@ -1,8 +1,16 @@
 <?php
+require_once 'ContactManager.php';
+
+
+$manager = new ContactManager();
+
 while (true) {
     $line = readline("Entrez votre commande : ");
     
     if($line === 'list'){
-        echo "affichage de la liste \n";
+        $contacts = $manager->findAll();
+        foreach ($contacts as $contact) {
+            echo $contact;
+        }
     }
 }
