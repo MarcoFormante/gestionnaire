0e0ec777444b852e6a893a922c3b5f8c8a20e880
Marco <formante.marco@gmail.com>
2025-09-28 11:57:50 +0200

    Create database, contact table and add some contacts

---
+++ queries-etape2.sql
@@ -0,0 +1,17 @@
+
+    CREATE DATABASE  IF NOT EXISTS gestionnaire
+
+
+    CREATE TABLE IF NOT EXISTS contact (
+    id int(11) PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    name varchar(50),
+    email varchar(255),
+    phone_number varchar(10)
+    )
+  
+
+    INSERT INTO contact(name, email, phone_number) VALUES
+    ('Mario Rossi', 'mario.rossi@example.com', '3201234567'),
+    ('Giulia Bianchi', 'giulia.bianchi@example.com', '3897654321'),
+    ('Luca Verdi', 'luca.verdi@example.com', '3339876543');
+
