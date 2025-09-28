e6e6c611e5346836b1cca0feb60414daab6590be
Marco <formante.marco@gmail.com>
2025-09-28 12:34:41 +0200

    Create Contact class with props and methods/getters and setters

---
+++ Contact.php
@@ -0,0 +1,54 @@
+<?php 
+
+class Contact
+{
+    private int $id;
+    private string $name;
+    private string $email;
+    private string $phone_number;
+
+
+    public function setId(int $id):void{
+        $this->id = $id;
+    }
+
+    public function getId():int{
+        return $this->id;
+    }
+
+
+
+    public function setName(string $name):void{
+        $this->name = $name;
+    }
+
+    public function getName():string{
+        return $this->name;
+    }
+
+
+
+    public function setEmail(string $email):void{
+        $this->email = $email;
+    }
+
+    public function getEmail():string{
+        return $this->email;
+    }
+
+
+
+    public function setPhoneNumber(string $phone_number):void{
+        $this->phone_number = $phone_number;
+    }
+
+    public function getPhoneNumber():string{
+        return $this->phone_number;
+    }
+
+
+    public function __toString()
+    {
+        return " ID: {$this->id}\n NAME: {$this->name}\n EMAIL: {$this->email}\n PHONE_NUMBER: {$this->phone_number}";
+    }
+}
