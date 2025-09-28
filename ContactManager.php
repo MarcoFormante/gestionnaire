e91646631aa493e35c755d512e8a77164c17642d
Marco <formante.marco@gmail.com>
2025-09-28 12:52:04 +0200

    Add connection to Database and fetch all contacts

--- ContactManager.php
+++ ContactManager.php
@@ -1,11 +1,37 @@
 <?php 
+require_once 'DBConnect.php';
+require_once 'Contact.php';
 
 class ContactManager 
 {
- 
-    public function findAll():array{
+    private $pdo;
 
-        return ["TEST"];
+    // Get PDO Connection from DBConnect Object when is created
+    public function __construct()
+    {
+        $DBConnect = new DBConnect();
+        $this->pdo = $DBConnect->getPDO();
+    }
+ 
+    //Find all contacts and return Contact Objects 
+    public function findAll():array{
+        $query = "SELECT * FROM contact";
+        $stmt = $this->pdo->prepare($query);
+        $stmt->execute();
+
+        $contacts = [];
+        while($row = $stmt->fetch(PDO::FETCH_ASSOC)){
+            $contact = new Contact();
+
+            $contact->setId($row['id']);
+            $contact->setName($row['name']);
+            $contact->setEmail($row['email']);
+            $contact->setPhoneNumber($row['phone_number']);
+
+            $contacts[] = $contact;
+        }
+
+        return $contacts;
     }
 }
 
